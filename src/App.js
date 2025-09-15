e9555fbdeb71a507ca374d277e3e4b077b982451
Saransh-Sangal20 <Saransh-Sangal20@users.noreply.github.com.>
2025-09-15 21:14:02 +0530

    Infinite scroll added, refactoring done, navbar fixed on top, top loading bar added

--- src/App.js
+++ src/App.js
@@ -1,102 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import NewsComponent from './components/NewsComponent';
-import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
-  return (
-    <>
-    <Router>
-      <Routes>
-        <Route path="/" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "general" category = "general" badgecolor = "bg-secondary"/>
-          </>
-        } />
-        <Route path="/general" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "general" category = "general" badgecolor="bg-secondary"/>
-          </>
-        } />
-        <Route path="/business" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "business" category = "business" badgecolor="bg-primary"/>
-          </>
-        } />
-        <Route path="/entertainment" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "entertainment" category = "entertainment" badgecolor="bg-danger"/>
-          </>
-        } />
-        <Route path="/health" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "health" category = "health" badgecolor="bg-success"/>
-          </>
-        } />
-        <Route path="/science" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "science" category = "science" badgecolor="bg-dark"/>
-          </>
-        } />
-        <Route path="/sports" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "sports" category = "sports" badgecolor="bg-warning"/>
-          </>
-        } />
-        <Route path="/technology" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "technology" category = "technology" badgecolor="bg-info"/>
-          </>
-        } />
-        <Route path="/india" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "india" category = "india"/>
-          </>
-        } />
-        <Route path="/us" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "us" category = "us"/>
-          </>
-        } />
-        <Route path="/japan" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "japan" category = "japan"/>
-          </>
-        } />
-        <Route path="/china" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "china" category = "china"/>
-          </>
-        } />
-        <Route path="/singapore" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "singapore" category = "singapore"/>
-          </>
-        } />
-        <Route path="/russia" element={
-          <>
-          <Navbar title="Daily Planet" />
-          <NewsComponent key = "russia" category = "russia"/>
-          </>
-        } />
-      </Routes>
-    </Router>
-    </>
-  );
+class App extends Component {
+  apikey = process.env.REACT_APP_NEWS_API_KEY;
+  state = {
+    progress: 0  // In modern react, state variables can be defined like this
+  };
+  setProgress = (value) => {
+    this.setState({progress: value});
+  };
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar title="Flash News" />  {/* Navbar is outside Routes, to stay on every page*/}
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+          />
+          <Routes>
+            {/* CATEGORY ROUTING */}
+            <Route path="/" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="general" category="general" badgecolor="bg-secondary" />} />
+            <Route path="/general" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="general" category="general" badgecolor="bg-secondary" />} />
+            <Route path="/business" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="business" category="business" badgecolor="bg-primary" />} />
+            <Route path="/entertainment" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="entertainment" category="entertainment" badgecolor="bg-danger" />} />
+            <Route path="/health" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="health" category="health" badgecolor="bg-success" />} />
+            <Route path="/science" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="science" category="science" badgecolor="bg-dark" />} />
+            <Route path="/sports" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="sports" category="sports" badgecolor="bg-warning" />} />
+            <Route path="/technology" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="technology" category="technology" badgecolor="bg-info" />} />
+
+            {/* COUNTRY ROUTING */}
+            <Route path="/india" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="india" category="india" />} />
+            <Route path="/us" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="us" category="us" />} />
+            <Route path="/japan" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="japan" category="japan" />} />
+            <Route path="/china" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="china" category="china" />} />
+            <Route path="/singapore" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="singapore" category="singapore" />} />
+            <Route path="/russia" element={<NewsComponent setProgress={this.setProgress} apikey={this.apikey}  key="russia" category="russia" />} />
+          </Routes>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -2,79 +2,115 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem';
 import download from './download.jpg';  // in case of no image from the news API. // in src folder, we can import images like this.
 import Loader from './Loader.js';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class NewsComponent extends Component {
-    capitalize(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-    }
-    constructor(props) {
-        super(props);
-        this.state= {
-            articles: [],
-            loading: false,
-            page: 1,
-            total: 0
-        };
-        document.title = `${this.capitalize(this.props.category)} - Daily Planet`;
-    } // states of class based component are always defined inside a constructor, inside [this.state] and as an object, and constructor must have a super() call.
-    // [this.articles] can be used instead of [this.state.articles] because articles is not going to change, but if it was going to change, then we would use [this.state.articles].
-    async componentDidMount() {
-        this.setState({loading: true}); // setting loading to true while fetching data from the API
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=25c27e6ef5004501a7befeadae4d2f5b&pageSize=6`);
-        let parsedData = await data.json();
-        this.setState({
-            articles: parsedData.articles,
-            total: parsedData.totalResults,
-            loading: false
-        })  // componentDidMount() is a lifecycle method that runs after the render method, and is used to fetch data from an API.
-    }
-    previousClick = async() => {
-      this.setState({loading: true});
-      let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=25c27e6ef5004501a7befeadae4d2f5b&pageSize=6&page=${this.state.page - 1}`);
-      let parsedData = await data.json();
-      this.setState({
-        articles: parsedData.articles,  // articles is array of objects
-        page: this.state.page - 1,
-        loading: false
-      }, () => {
-        window.scrollTo(0, 0); // scrolls to the top of the page when previous button is clicked
-      }); // setState takes a second argument which is a callback function that runs after the state is updated
-    }
-    nextClick = async () => {
-      if (this.state.page + 1 > Math.ceil(this.state.total/6)) {
-        // this usually wont work since we have already set disabled attribute in the next button
-      }
-      else {
-        this.setState({loading: true});
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=25c27e6ef5004501a7befeadae4d2f5b&pageSize=6&page=${this.state.page + 1}`);
-        let parsedData = await data.json();
-        this.setState({
-          articles: parsedData.articles,
-          page: this.state.page + 1,
-          loading: false
-        }, () => {
-          window.scrollTo(0, 0); // scrolls to the top of the page when next button is clicked
-        });  // ensuring that window scrolls to top after next button is clicked
-      }
-    }
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      total: 0
+    };
+    document.title = `${this.capitalize(this.props.category)} - Flash News`;
+  } // states of class based component are always defined inside a constructor, inside [this.state] and as an object, and constructor must have a super() call.
+  // [this.articles] can be used instead of [this.state.articles] if articles is not a state variable (not wriiten in this.state), but if it is a state variable, then we would use [this.state.articles].
+  async componentDidMount() {
+    this.setState({ loading: true }); // setting loading to true while fetching data from the API
+    this.props.setProgress(10);
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&pageSize=6`);
+    this.props.setProgress(30);
+    let parsedData = await data.json();
+    this.props.setProgress(50);
+    this.setState({
+      articles: parsedData.articles,
+      total: parsedData.totalResults,
+      loading: false
+    })  // componentDidMount() is a lifecycle method that runs after the render method, and is used to fetch data from an API.
+    this.props.setProgress(100);
+  }  // cdm function is a lifecycle method, so it belongs to class itself and therefore "this" keyword is binded with it, so no need to make it an arrow function.
+
+  // previousClick = async() => {
+  //   this.setState({loading: true});
+  //   this.props.setProgress(10);
+  //   let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&pageSize=6&page=${this.state.page - 1}`);
+  //   this.props.setProgress(30);
+  //   let parsedData = await data.json();
+  //   this.props.setProgress(50);
+  //   this.setState({
+  //     articles: parsedData.articles,  // articles is array of objects
+  //     page: this.state.page - 1,
+  //     loading: false
+  //   }, () => {
+  //     window.scrollTo(0, 0); // scrolls to the top of the page when previous button is clicked
+  //   }); // setState takes a second argument which is a callback function that runs after the state is updated
+  //   this.props.setProgress(100);
+  // }
+
+  // nextClick = async () => {
+  //   if (this.state.page + 1 > Math.ceil(this.state.total/6)) {
+  //     // this usually wont work since we have already set disabled attribute in the next button
+  //   }
+  //   else {
+  //     this.setState({loading: true});
+  //     this.props.setProgress(10);
+  //     let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&pageSize=6&page=${this.state.page + 1}`);
+  //     this.props.setProgress(30);
+  //     let parsedData = await data.json();
+  //     this.props.setProgress(50);
+  //     this.setState({
+  //       articles: parsedData.articles,
+  //       page: this.state.page + 1,
+  //       loading: false
+  //     }, () => {
+  //       window.scrollTo(0, 0); // scrolls to the top of the page when next button is clicked
+  //     });  // ensuring that window scrolls to top after next button is clicked
+  //     this.props.setProgress(100);
+  //   }
+  // }
+
+  fetchMoreData = async () => {
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&pageSize=6&page=${this.state.page + 1}`);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      page: this.state.page + 1,
+    });
+  };
+
   render() {
     return (
-      <div className='container my-4 ms-auto ps-5'>
-        <h2 className='text-center'>Daily Planet's - Top {this.capitalize(this.props.category)} Headlines</h2>
+      <>
+        <h2 className='text-center' style={{marginTop: "90px"}}>Flash News - Top {this.capitalize(this.props.category)} Headlines</h2>
         {this.state.loading && <Loader />} {/* if loading is true, then show the loader */}
-        {!this.state.loading && <div className="row mt-4">
-            {this.state.articles.map((element)=> {
+        <InfiniteScroll
+          dataLength={this.state.articles.length}  // defines length of articles that is to be fetched
+          next={this.fetchMoreData}  // inside is a function to fetch next page data on scrolling down
+          hasMore={this.state.articles.length < this.state.total}  // condition to stop fetching more articles on scrolling down
+          loader={<Loader />}
+        >
+          <div className='container my-4 ms-auto ps-5'>
+            {<div className="row mt-4">
+              {this.state.articles.map((element) => {
                 return <div className="col-4" key={element.url}>
-                    <NewsItem title={element.title} author={element.author} date={element.publishedAt} source={element.source.name} badgecolor={this.props.badgecolor} description={element.description} imgUrl={element.urlToImage?element.urlToImage:download} newsUrl={element.url}/>
+                  <NewsItem title={element.title} author={element.author} date={element.publishedAt} source={element.source.name} badgecolor={this.props.badgecolor} description={element.description} imgUrl={element.urlToImage ? element.urlToImage : download} newsUrl={element.url} />
                 </div>
-            })}
-        </div>}
-        <div className='d-flex justify-content-around my-4'>
+              })}
+            </div>}
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className='d-flex justify-content-around my-4'>
           <button type="button" disabled={this.state.page===1} className="btn btn-dark" onClick={this.previousClick}> &larr; Previous</button>
           <h4>Page: {this.state.page} of {Math.ceil(this.state.total/6)}</h4>
           <button type="button" disabled={this.state.page===Math.ceil(this.state.total/6)} className="btn btn-dark" onClick={this.nextClick}>Next &rarr;</button>
-        </div>
-      </div>
+        </div> */}
+
+      </>
     )
   }
 }
